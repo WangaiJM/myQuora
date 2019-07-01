eca3d2008e0b9668757de0c8d88cc6bbb57f56ad
WangaiJM <jahkeyjohn@gmail.com>
2019-07-01 16:21:23 +0300

    Database seeding and factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 5)->create()->each(function($user){
+            $user->posts()->saveMany(
+                factory(App\Question::class, rand(4,7))->make()
+            );
+        });
     }
 }
